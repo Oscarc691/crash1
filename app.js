efc856ab1138eed9e2dc11a036008b94db80385a
Oscar Campos <oscarc691@gmail.com>
2024-04-25 17:11:16 -0600

    codes

---
+++ app.js
@@ -0,0 +1,8 @@
+function sumGreaterThan100(num1 , num2) {
+ return num1 + num2 > 100 }
+ console.log(sumGreaterThan100(50,52));
+
+ function lessThanOrEqualToZero(num1) {
+    return num1 <= 0;
+ }
+ console.log(lessThanOrEqualToZero(3, 0 ,-2));
